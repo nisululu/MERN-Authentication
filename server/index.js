375dea44fbd111dd848e26fd6f4110a795dddfe0
N33XAL <nischaltandukar0@gmail.com>
2024-03-13 14:36:07 +0545

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config({ path: '../.env' });
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Database connected"))
+    .catch((error) => console.log(error))
 
 const app = express()
 
-app.listen(3000, ()=>{
-    console.log("server listening to port 300");
+app.listen(3000, () => {
+    console.log("server listening to port 3000");
 })
