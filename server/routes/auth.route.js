68d28b6a0ba76b8cb2eca7df580275843d47c02a
N33XAL <nischaltandukar0@gmail.com>
2024-03-14 15:47:45 +0545

    updated signup route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -2,6 +2,6 @@ const express = require('express')
 const {signup} = require('../controller/auth.controller')
 const router = express.Router()
 
-router.post('/',signup)
+router.post('/signup',signup)
 
 module.exports = router
