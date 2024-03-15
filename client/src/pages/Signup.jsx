8cf8454f1e1386f5a047599ccae0bf9fcdd5e104
N33XAL <nischaltandukar0@gmail.com>
2024-03-15 15:16:35 +0545

    Completed signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 
+
 const Signup = () => {
+
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(null)
+
+  const handleChange = (e) => {
+    setFormData({...formData,[e.target.name] : e.target.value})
+    setError(null)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()    
+
+    try{
+      setLoading(true)
+      setError(false)
+      const config = { headers: { "Content-Type": "application/json" } }
+      const {data} = await axios.post('/api/auth/signup',formData, config)
+      if(data){
+        console.log(data);
+        setLoading(false)
+      }
+    }catch(err) {
+      setLoading(false)
+      setError(true)
+      console.log(err.response.data);
+    }
+  }
+
   return (
     <div className='min-h-full flex flex-col justify-center px-6 pt-20 lg:px-8'>
       
@@ -10,11 +41,13 @@ const Signup = () => {
       </div>
 
       <div className='mt-10 max-w-sm sm:mx-auto sm:w-full sm:max-w-md'>
-        <form className='space-y-5'>
+        <form className='space-y-5' onSubmit={handleSubmit}>
           <div>
           <label className='font-medium text-small text-gray-900'>User Name</label>
             <div className="mt-2">
               <input
+                onChange={handleChange}
+                name="username"
                 type="text"
                 required
                 className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -26,6 +59,8 @@ const Signup = () => {
             <label className='font-medium text-small text-gray-900'>Email</label>
             <div className="mt-2">
               <input
+                onChange={handleChange}
+                name='email'
                 type="email"
                 required
                 className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -37,6 +72,8 @@ const Signup = () => {
           <label className='font-medium text-small text-gray-900'>Password</label>
             <div className="mt-2">
               <input
+                onChange={handleChange}
+                name='password'
                 type="password"
                 required
                 className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -49,7 +86,7 @@ const Signup = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign up
+                {loading? "Loading..." : "Sign up"}
               </button>
             </div>
 
@@ -65,6 +102,10 @@ const Signup = () => {
             <div className='flex justify-between'>
               <p>Have an account?</p>
               <Link to='/sign-in'>Sign in</Link>
+            </div>
+
+            <div>
+              {error && <p className='text-red-700'>Ops, something went wrong.</p>}
             </div>
         </form>
       </div>
