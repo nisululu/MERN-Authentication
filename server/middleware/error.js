5139186c7be0e1427b435f9648a82786ec96aafa
N33XAL <nischaltandukar0@gmail.com>
2024-03-15 15:12:04 +0545

    created middleware to handle errors

---
+++ server/middleware/error.js
@@ -0,0 +1,19 @@
+const errorHandler = require('../utils/error')
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500
+    err.message = err.message || "Internal Server Error"
+
+    console.log(err);
+
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+        err = errorHandler(400, message)
+    }
+
+    res.status(err.statusCode).json({
+        success: false,
+        message: err.message,
+        statusCode: err.statusCode
+    })
+}
