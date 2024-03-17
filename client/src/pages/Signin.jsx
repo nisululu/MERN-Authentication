f6db93fcf9fa4856551507f912367cc95daec720
N33XAL <nischaltandukar0@gmail.com>
2024-03-17 16:57:28 +0545

    created signin page and added authentication

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Signin = () => {
+
+  const [formData, setFormData] = useState({})
+  const [loading, setLoading] = useState(null)
+  const [error, setError] = useState(null)
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+    setLoading(null)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try{
+      const config = { headers: { "Content-Type": "application/json" } }
+      const { data } = await axios.post('/api/auth/signin', formData, config)
+
+      console.log(data);
+
+    }catch(err) {
+      console.log(err.response.data);
+    }
+  }
+
   return (
-    <div>
-      
+    <div className='min-h-full flex flex-col justify-center px-6 pt-20 lg:px-8'>
+
+      <div>
+        <h2 className='text-center text-2xl font-bold leading-9 tracking-tight'>Sign in</h2>
+      </div>
+
+      <div className='mt-10 max-w-sm sm:mx-auto sm:w-full sm:max-w-md'>
+        <form className='space-y-5' onSubmit={handleSubmit}>
+
+          <div>
+            <label className='font-medium text-small text-gray-900'>Email</label>
+            <div className="mt-2">
+              <input
+                onChange={handleChange}
+                name='email'
+                type="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label className='font-medium text-small text-gray-900'>Password</label>
+            <div className="mt-2">
+              <input
+                onChange={handleChange}
+                name='password'
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {/* {loading ? "Loading..." : "Sign in"} */}
+              Sign in
+            </button>
+          </div>
+
+          <div>
+            {/* {error && <p className='text-red-700'>Ops, something went wrong.</p>} */}
+          </div>
+        </form>
+      </div>
+
     </div>
   )
 }
--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,16 +1,40 @@
 const User = require("../models/User.model")
 const bcryptjs = require('bcryptjs')
 const errorHandler = require('../utils/error')
-exports.signup = async(req, res, next) => {
-    const {username, email, password} = req.body
-    const hashedPass = bcryptjs.hashSync(password, 10)
-    const newUser = new User({username, email, password: hashedPass})
-    try{
+const jwt = require('jsonwebtoken')
+
+exports.signup = async (req, res, next) => {
+    const { username, email, password } = req.body
+    const hashedPass = bcryptjssh.haSync(password, 10)
+    const newUser = new User({ username, email, password: hashedPass })
+
+    try {
         await newUser.save()
         res.status(200).json({
-            message: "User Successfully added",
-            newUser})
-    }catch(err){
+            message: "User added Successfully",
+            newUser
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+exports.signin = async (req, res, next) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+        const comparePassword = bcryptjs.compareSync(password, user.password)
+        
+        if (!user) return next(errorHandler(400, "User not Found")) 
+        if(!comparePassword) return next(errorHandler(401, "Wrong credentials."))
+
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+        const {password: hashedPassword, ...rest} = user._doc
+        
+        res.cookie('token',token, {httpOnly: true, expires: new Date(Date.now() + 3600000)}).json(rest)
+
+    } catch (err) {
         next(err)
     }
 }
