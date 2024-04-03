3b4931bfcddd1b164e95388f813ac7d09f69609d
N33XAL <nischaltandukar0@gmail.com>
2024-04-03 15:43:54 +0545

    fixing typo error

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/User.model")
+const User = require("../models/user.model")
 const bcryptjs = require('bcryptjs')
 const errorHandler = require('../utils/error')
 const jwt = require('jsonwebtoken')
